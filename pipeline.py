941bff7482e6a36facfdb3c1b85eb095a8c4379a
Anshuman Tiwari <lazycodebaker@outlook.com>
2025-03-18 16:23:41 +0530

    changes

--- pipeline.py
+++ pipeline.py
@@ -47,16 +47,21 @@ class AlertSystem:
 
         return formatted_alerts
 
-    def send_log_to_db(self, message: str, data: Dict[str, Any]):
-        try:
-            log_entry = BaylinkAlertLogs(
-                message=message, 
-                person_name=data['person_name'],
-                role=data['role']
-            )
-            self.session.add(log_entry)
-            self.session.commit()
-            print(f"Alert logged to DB")
+    def send_log_to_db(self, messages: List[str], data: Dict[str, Any]):
+        try: 
+            for message in messages:
+                
+                log_entry = BaylinkAlertLogs(
+                    message=message, 
+                    person_name=data['person_name'],
+                    role=data['role'],
+                    recepient = data['recepient']
+                )
+                self.session.add(log_entry)
+                self.session.commit()
+                
+                send_alert(message,"7007555103" ) # data['recepient'])
+                print(f"Alert logged to DB")
         except Exception as e:
             self.session.rollback()
             print(f"Failed to log alert to DB: {e}")
@@ -65,6 +70,7 @@ class AlertSystem:
         
     def alert_pipeline(self, event_name: str, event_data: Dict[str, Any]): 
         try:
+            '''
             alerts = []
             if event_name in event_config.keys():  
                 conditions = event_config[event_name]
@@ -87,24 +93,42 @@ class AlertSystem:
             alerts = self.group_alerts_by_recipient(alerts)
             
             with open("alerts.json", "w") as f:
-                json.dump(alerts, f)
+                json.dump(alerts, f, indent=4)
+            '''
                 
-            return
+            alerts = [
+                    {
+                        "recipient": "8285027775",
+                        "messages": [
+                            "Stock of Jamun Masala- Discounted is expiring soon. Please take necessary action.",
+                            "Stock of Original Lemon Masala Juice is expiring soon. Please take necessary action.",
+                            "BeatPlan not assigned to Rohit, Raunak, Anish Kumar, Naina, Prakash Jha, Aneesh Singh, Anurag Tiwari, Asmit Agarwal, Manoj, Siddharth, Deepak, Sandeep singh, Sudhanshu Dixit, Aneesh Singh, Pranav, Goldy, Anil Kumar , Akash yadav, Aman, Sakshat, Sakina Khan, Arpit, Mukesh for today."
+                        ],
+                        "person_name": "Deepak",
+                        "role": "ASM"
+                    },
+                    {
+                        "recipient": "8447686869",
+                        "messages": [
+                            "Stock of LZB Product is expiring soon. Please take necessary action.",
+                            "Stock of Jamun Masala- Discounted is expiring soon. Please take necessary action.",
+                            "Stock of Original Lemon Masala Juice is expiring soon. Please take necessary action.",
+                            "BeatPlan not assigned to Vinay Singh for today."
+                        ],
+                        "person_name": "Siddharth Prakash",
+                        "role": "ASM"
+                    }
+                ]
                         
             for alert in alerts:
-                recepient = alert["recepient"]
-                person_name = alert["person_name"]
-                role = alert["role"] 
-                message = alert["message"] 
-                
-                self.send_log_to_db(message, {
-                    "person_name": person_name,
-                    "role": role
-                })
-                
-                #send_alert(messages, "7007555103") # recepient
+              recipient = alert['recipient']
+              messages = alert['messages']
+              person_name = alert['person_name']
+              role = alert['role']
+              self.send_log_to_db(messages, {"recepient": recipient, "person_name": person_name, "role": role})
         
         except Exception as e:
             print(f"Alert Pipeline Failed: {e}")    
             
 alert_system = AlertSystem() 
+alert_system.alert_pipeline("",{})
