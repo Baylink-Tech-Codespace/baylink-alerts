632051969b8e799ea7905bb609b6b2abbaa7988e
Anshuman Tiwari <lazycodebaker@outlook.com>
2025-03-27 18:03:08 +0530

    changes

--- event/main.py
+++ event/main.py
@@ -202,7 +202,7 @@ class Monitor:
         schedule.clear()
         
         # Schedule jobs
-        schedule.every().day.at("16:40").do(self.daily_event_triggers)
+        schedule.every().day.at("17:14").do(self.daily_event_triggers)
         schedule.every(30).days.at("11:18").do(self.monthly_event_triggers)
         
         print("Scheduler started for daily/monthly triggers...")
